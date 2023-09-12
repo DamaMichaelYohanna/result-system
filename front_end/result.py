1e408904f4202f26c76093d28ed5475d471423f8
Dama Michael Yohanna <get2dama11@gmail.com>
2023-09-12 19:34:34 +0100

    update result sections

--- front_end/result.py
+++ front_end/result.py
@@ -1,9 +1,9 @@
 from PySide6.QtWidgets import QPushButton, QLabel, \
     QLineEdit, QComboBox, QFrame, QVBoxLayout, QHBoxLayout, \
-    QHeaderView, QTableWidget, QTableWidgetItem,QMessageBox
+    QHeaderView, QTableWidget, QTableWidgetItem, QMessageBox
 
-from PySide6.QtGui import QIcon
-
+from PySide6.QtGui import Qt
+from PySide6 import QtCore
 from draw_line import QHSeparationLine, QVSeparationLine
 from new_student import RegisterUI
 
@@ -14,13 +14,21 @@ class Result(QFrame):
         super(Result, self).__init__()
         main_layout = QVBoxLayout()
         menu_layout = QHBoxLayout()
-        filter_label = QLabel("Filter By Class")
-        filter_label.setStyleSheet("padding:4px;font-size:15px;")
-        filter_input = QComboBox()
+        class_filter = QLabel("Class Filter")
+        class_filter.setStyleSheet("padding:4px;font-size:15px;")
+        class_filter_input = QComboBox()
         text_list = ["All", "JSS 1", "JSS 2", "JSS 3", "SSS 1"]
-        filter_input.addItems(text_list)
-        filter_input.setStyleSheet("padding:4px;font-size:15px;")
-        filter_input.setMinimumWidth(200)
+        class_filter_input.addItems(text_list)
+        class_filter_input.setStyleSheet("padding:4px;font-size:15px;")
+        class_filter_input.setMinimumWidth(200)
+
+        subject_filter = QLabel("Subject Filter")
+        subject_filter.setStyleSheet("padding:4px;font-size:15px;")
+        subject_filter_input = QComboBox()
+        subject_list = ["All", "JSS 1", "JSS 2", "JSS 3", "SSS 1"]
+        subject_filter_input.addItems(text_list)
+        subject_filter_input.setStyleSheet("padding:4px;font-size:15px;")
+        subject_filter_input.setMinimumWidth(200)
         # filter_input.currentTextChanged.connect("me")
         self.search_input = QLineEdit()
         self.search_input.setPlaceholderText("Find A Student")
@@ -33,42 +41,81 @@ class Result(QFrame):
         # add_button.setStyleSheet("padding:8px;border-radius:0px;"
         #                          "background:rgb(14, 180, 166);color:white;font-weight:bold;")
 
-        menu_layout.addWidget(filter_label)
-        menu_layout.addWidget(filter_input)
+        menu_layout.addWidget(class_filter)
+        menu_layout.addWidget(class_filter_input)
+        menu_layout.addWidget(subject_filter)
+        menu_layout.addWidget(subject_filter_input)
+        menu_layout.addWidget(QVSeparationLine())
         menu_layout.addWidget(self.search_input)
         menu_layout.addWidget(search_button)
         menu_layout.addWidget(QVSeparationLine())
         # menu_layout.addWidget(add_button)
         # menu_layout.addWidget()
 
-        table = QTableWidget()
-        table.setColumnCount(6)
-        table.setRowCount(2)
+        self.table = QTableWidget()
+        self.table.setColumnCount(6)
+        self.table.setRowCount(5)
         # table.setColumnWidth(0)
-        table.setHorizontalHeaderLabels(["Name", "Class", "First CA", "Second CA", "Assignment", "Exams"])
-        table.setStyleSheet("QTableWidget::item {border: 10px; padding: 5px;color:red;font-style:bolder;}")
+        self.table.setHorizontalHeaderLabels(["Name", "Class", "First CA", "Second CA", "Assignment", "Exams"])
+        self.table.setStyleSheet(
+            "QTableWidget::item {font-size:18px;selection-background-color:#f5f5f5;selection-color:black;}"
+            "QHeaderView {font-size:18px;}")
+        # table.horizontalHeader().setStyleSheet("QHeaderView {font-size:40px};")
+        self.table.horizontalHeader().setDefaultAlignment(QtCore.Qt.AlignLeft)
         student = [["Dama Michael Yohanna ", "jss1", "20", "20", "20", "90"],
                    ["dama", "jss1", "20", "kaduna", "jamaa", "100"],
                    ["dama", "jss1", "20", "kaduna", "jamaa", "100"],
                    ["dama", "jss1", "20", "kaduna", "jamaa", "100"],
                    ["dama", "jss1", "20", "kaduna", "jamaa", "100"]]
-        table.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
+        self.table.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
         start = 0
         for index, value in enumerate(student):
-            table.setItem(index, 0, QTableWidgetItem(student[index][0]))
-            table.setItem(index, 1, QTableWidgetItem(student[index][1]))
-            table.setItem(index, 2, QTableWidgetItem(student[index][2]))
-            table.setItem(index, 3, QTableWidgetItem(student[index][3]))
-            table.setItem(index, 4, QTableWidgetItem(student[index][4]))
-            table.setItem(index, 4, QTableWidgetItem(student[index][5]))
+            item1 = QTableWidgetItem(student[index][0])
+            item1.setFlags(Qt.ItemIsEnabled)
+            self.table.setItem(index, 0, item1)
+            self.table.setItem(index, 1, QTableWidgetItem(student[index][1]))
+            self.table.setItem(index, 2, QTableWidgetItem(student[index][2]))
+            self.table.setItem(index, 3, QTableWidgetItem(student[index][3]))
+            self.table.setItem(index, 4, QTableWidgetItem(student[index][4]))
+            self.table.setItem(index, 4, QTableWidgetItem(student[index][5]))
 
+        action_layout = QHBoxLayout()
+        clear_btn = QPushButton("Clear All")
+        clear_btn.clicked.connect(self.clear_btn_callback)
+        finish_btn = QPushButton("Save Record")
+        finish_btn.clicked.connect(self.finish_btn_callback)
+        action_layout.addWidget(clear_btn)
+        action_layout.addWidget(finish_btn)
+        # arrange widget to main layout
         main_layout.addLayout(menu_layout)
         main_layout.addWidget(QHSeparationLine())
-        main_layout.addWidget(table)
-
-        # main_layout.addStretch()
+        main_layout.addWidget(self.table)
+        main_layout.addLayout(action_layout)
 
         self.setLayout(main_layout)
+
+    def finish_btn_callback(self):
+        for row in range(self.table.rowCount()):
+            for column in range(self.table.columnCount()):
+                value = self.table.item(row, column)
+                if value:
+                    print(value.text(), end=" ")
+                else:
+                    continue
+            print()
+
+    def clear_btn_callback(self):
+        """call back function to clear the entries """
+        accept = QMessageBox.question(self, "Warning", "Are you sure you want to clear? "
+                                                       "Scores will be gone")
+        if accept == 16384:
+            for row in range(self.table.rowCount()):
+                for column in range(2, self.table.columnCount()):
+                    value = self.table.item(row, column)
+                    if value:
+                        value.setText("")
+                    else:
+                        continue
 
     def add_student_callback(self):
         print("hello")
@@ -77,8 +124,8 @@ class Result(QFrame):
 
     def delete_student_callback(self):
         me = QMessageBox.question(self, "Warning", "Are you sure you want to delete this student?"
-                                    " You will lose all the student result after this"
-                                    " action. You can Hide instead",)
+                                                   " You will lose all the student result after this"
+                                                   " action. You can Hide instead", )
         print("i was clicked")
 
     def update_student_callback(self):
