7e2fc31dc7c6b0a71d5106e31d59eb514f134a36
Dama Michael Yohanna <get2dama11@gmail.com>
2023-09-14 12:40:06 +0100

    updated new student page

--- front_end/new_student.py
+++ front_end/new_student.py
@@ -10,6 +10,9 @@ class RegisterUI(widget.QDialog):
         super(RegisterUI, self).__init__(parent)
         self.setFixedWidth(620)
         self.setWindowTitle("New Student")
+        with open("student.qss") as file:
+            style = file.read()
+        self.setStyleSheet(style)
         self.build_ui()
 
     def clear_btn_callback(self):
@@ -18,15 +21,15 @@ class RegisterUI(widget.QDialog):
             self.name_entry.setText("")
             self.class_entry.setText("")
             self.age_entry.setText("")
-            self.lga_entry.setText("")
+            self.state_entry.setText("")
             self.lga_entry.setText("")
 
     def save_button_callback(self):
         name = self.name_entry.text()
         class_ = self.class_entry.text()
         age = self.age_entry.text()
+        state = self.state_entry.text()
         lga = self.lga_entry.text()
-        state = self.lga_entry.text()
         if not name or not class_ or not age or not lga or not state:
             QMessageBox.information(self, "Failure", 'Fields cannot be empty!')
         else:
@@ -44,7 +47,6 @@ class RegisterUI(widget.QDialog):
 
         layout = widget.QVBoxLayout()
         inner_frame = widget.QFrame()
-        inner_frame.setStyleSheet("background:white;")
         inner_frame.setObjectName("add_student_frame")
         inner_layout = widget.QVBoxLayout()
         inner_frame.setFixedWidth(600)
@@ -52,55 +54,35 @@ class RegisterUI(widget.QDialog):
         self.name_label = widget.QLabel("Student Name")
         self.name_label.setStyleSheet("""margin-top:10px;font-size:15px;""")
         self.name_entry = widget.QLineEdit()
-        self.name_entry.setStyleSheet(
-            """
-            padding:3px;font-size:15px;
-            color:gray;margin-top:10px;
-            border-radius:1px;border:1px solid grey;
-            """)
+        self.name_entry.setObjectName("entry")
         class_label = widget.QLabel("Student Class")
         class_label.setStyleSheet("""margin-top:10px;font-size:15px;""")
         self.class_entry = widget.QLineEdit()
-        self.class_entry.setStyleSheet(
-            """
-            padding:3px;font-size:15px;
-            color:gray;margin-top:10px;
-            border-radius:1px;border:1px solid grey;""")
+        self.class_entry.setObjectName("entry")
         age_label = widget.QLabel("Student Age")
         age_label.setStyleSheet("""margin-top:10px;font-size:15px;padding-left:0px;""")
         self.age_entry = widget.QLineEdit()
-        self.age_entry.setStyleSheet(
-            """
-            padding:3px;font-size:15px;
-            color:gray;margin-top:10px;
-            border-radius:1px;border:1px solid grey;
-            """)
+        self.age_entry.setObjectName("entry")
         state_label = widget.QLabel("Student's State")
         state_label.setStyleSheet("""margin-top:10px;font-size:15px;""")
         self.state_entry = widget.QLineEdit()
-        self.state_entry.setStyleSheet(
-            """
-            padding:3px;font-size:15px;
-            color:gray;margin-top:10px;
-            border-radius:1px;border:1px solid grey;""")
+        self.state_entry.setObjectName("entry")
+
         lga_label = widget.QLabel("Student's LGA")
         lga_label.setStyleSheet("""margin-top:10px;font-size:15px;""")
+
         self.lga_entry = widget.QLineEdit()
-        self.lga_entry.setStyleSheet(
-            """
-            padding:3px;font-size:15px;
-            color:gray;margin-top:10px;
-            border-radius:1px;border:1px solid grey;""")
+        self.lga_entry.setObjectName("entry")
         action_layout = widget.QHBoxLayout() # create new layout for button
         clear_btn = widget.QPushButton("Clear Fields")
-        clear_btn.setStyleSheet(
-            "background:rgb(14, 180, 166);margin-top:20px;padding:10px;border-radius:5px")
+        clear_btn.setObjectName("action_button")
         clear_btn.clicked.connect(self.clear_btn_callback)
+
         submit_btn = widget.QPushButton("Add Student")
-        submit_btn.setStyleSheet(
-            "background:rgb(14, 180, 166);margin-top:20px;padding:10px;border-radius:5px")
+        submit_btn.setObjectName("action_button")
         submit_btn.setFixedWidth(300)
         submit_btn.clicked.connect(self.save_button_callback)
+
         action_layout.addWidget(clear_btn)
         action_layout.addWidget(submit_btn)
         # --------------------------- A add widget to layout
