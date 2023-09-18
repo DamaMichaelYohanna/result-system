3c5f8d51d4d6567bce49528ac00d96ab14af3317
Dama Michael Yohanna <get2dama11@gmail.com>
2023-09-18 09:43:03 +0100

    added few state for display

--- front_end/extra.py
+++ front_end/extra.py
@@ -1,461 +1,319 @@
-{
-   "Abia State": {
-      "Aba North": {},
-      "Aba South": {},
-      "Arochukwu": {},
-      "Bende": {},
-      "Ikwuano": {},
-      "Isiala Ngwa North": {},
-      "Isiala Ngwa South": {},
-      "Isuikwuato": {},
-      "Obi Ngwa": {},
-      "Ohafia": {},
-      "Osisioma Ngwa": {},
-      "Ugwunagbo": {},
-      "Ukwa East": {},
-      "Ukwa West": {},
-      "Umuahia North": {},
-      "Umuahia South": {},
-      "Umu Nneochi": {}
-   }
-}
-{
-  "Adamawa State": {
-    "LGAs": [
-      "Demsa",
-      "Fufore",
-      "Ganye",
-      "Girei",
-      "Gombi",
-      "Guyuk",
-      "Hong",
-      "Jada",
-      "Lamurde",
-      "Madagali",
-      "Maiha",
-      "Mayo-Belwa",
-      "Michika",
-      "Mubi North",
-      "Mubi South",
-      "Numan",
-      "Shelleng",
-      "Song",
-      "Toungo",
-      "Yola North",
-      "Yola South"
+states = {
+    "Abia State": [
+        "Aba North",
+        "Aba South",
+        "Arochukwu",
+        "Bende",
+        "Ikwuano",
+        "Isiala Ngwa North",
+        "Isiala Ngwa South",
+        "Isuikwuato",
+        "Obi Ngwa",
+        "Ohafia",
+        "Osisioma Ngwa",
+        "Ugwunagbo",
+        "Ukwa East",
+        "Ukwa West",
+        "Umuahia North",
+        "Umuahia South",
+        "Umu Nneochi",
+    ],
+    "Adamawa State": [
+        "Demsa",
+        "Fufore",
+        "Ganye",
+        "Girei",
+        "Gombi",
+        "Guyuk",
+        "Hong",
+        "Jada",
+        "Lamurde",
+        "Madagali",
+        "Maiha",
+        "Mayo-Belwa",
+        "Michika",
+        "Mubi North",
+        "Mubi South",
+        "Numan",
+        "Shelleng",
+        "Song",
+        "Toungo",
+        "Yola North",
+        "Yola South"
+    ],
+    "Akwa Ibom State": [
+        "Abak",
+        "Eastern Obolo",
+        "Eket",
+        "Esit Eket",
+        "Essien Udim",
+        "Etim Ekpo",
+        "Etinan",
+        "Ibeno",
+        "Ibesikpo Asutan",
+        "Ibiono Ibom",
+        "Ika",
+        "Ikono",
+        "Ikot Abasi",
+        "Ikot Ekpene",
+        "Ini",
+        "Itu",
+        "Mbo",
+        "Mkpat Enin",
+        "Nsit Atai",
+        "Nsit Ibom",
+        "Nsit Ubium",
+        "Obot Akara",
+        "Okobo",
+        "Onna",
+        "Oron",
+        "Oruk Anam",
+        "Udung Uko",
+        "Ukanafun",
+        "Uruan",
+        "Urue-Offong/Oruko",
+        "Uyo"
+    ],
+    "Anambra": [
+        "Aguata",
+        "Awka North",
+        "Awka South",
+        "Anambra East",
+        "Anambra West",
+        "Anaocha",
+        "Ayamelum",
+        "Dunukofia",
+        "Ekwusigo",
+        "Idemili North",
+        "Idemili South",
+        "Ihiala",
+        "Njikoka",
+        "Nnewi North",
+        "Nnewi South",
+        "Ogbaru",
+        "Onitsha North",
+        "Onitsha South",
+        "Orumba North",
+        "Orumba South",
+        "Oyi"
+    ],
+    "Bauchi State": [
+        "Alkaleri",
+        "Bauchi",
+        "Bogoro",
+        "Damban",
+        "Darazo",
+        "Dass",
+        "Ganjuwa",
+        "Giade",
+        "Itas/Gadau",
+        "Jama'are",
+        "Katagum",
+        "Kirfi",
+        "Misau",
+        "Ningi",
+        "Shira",
+        "Tafawa Balewa",
+        "Toro",
+        "Warji",
+        "Zaki",
+    ],
+    "Bayelsa": [
+        "Brass",
+        "Ekeremor",
+        "Kolokuma/Opokuma",
+        "Nembe",
+        "Ogbia",
+        "Sagbama",
+        "Southern Ijaw",
+        "Yenagoa"
     ]
-  }
-}
-{
-   "Akwa Ibom State": {
-      "Abak": {
-         "capital": "Abak",
-         "population": "N/A"
-      },
-      "Eastern Obolo": {
-         "capital": "Okoroete",
-         "population": "N/A"
-      },
-      "Eket": {
-         "capital": "Eket",
-         "population": "N/A"
-      },
-      "Esit Eket": {
-         "capital": "Afaha Eket",
-         "population": "N/A"
-      },
-      "Essien Udim": {
-         "capital": "Ikot Okoro",
-         "population": "N/A"
-      },
-      "Etim Ekpo": {
-         "capital": "Ututu",
-         "population": "N/A"
-      },
-      "Etinan": {
-         "capital": "Etinan",
-         "population": "N/A"
-      },
-      "Ibeno": {
-         "capital": "Ibeno",
-         "population": "N/A"
-      },
-      "Ibesikpo Asutan": {
-         "capital": "Nung Udoe",
-         "population": "N/A"
-      },
-      "Ibiono Ibom": {
-         "capital": "Okop Ndua Erong",
-         "population": "N/A"
-      },
-      "Ika": {
-         "capital": "Ika",
-         "population": "N/A"
-      },
-      "Ikono": {
-         "capital": "Ibiaku Ntong",
-         "population": "N/A"
-      },
-      "Ikot Abasi": {
-         "capital": "Ikot Abasi",
-         "population": "N/A"
-      },
-      "Ikot Ekpene": {
-         "capital": "Ikot Ekpene",
-         "population": "N/A"
-      },
-      "Ini": {
-         "capital": "Odoro Ikpe",
-         "population": "N/A"
-      },
-      "Itu": {
-         "capital": "Itu",
-         "population": "N/A"
-      },
-      "Mbo": {
-         "capital": "Enwang",
-         "population": "N/A"
-      },
-      "Mkpat Enin": {
-         "capital": "Ikot Akpa Nkuk",
-         "population": "N/A"
-      },
-      "Nsit Atai": {
-         "capital": "Nsit Atai",
-         "population": "N/A"
-      },
-      "Nsit Ibom": {
-         "capital": "Nsit Ibom",
-         "population": "N/A"
-      },
-      "Nsit Ubium": {
-         "capital": "Nsit Ubium",
-         "population": "N/A"
-      },
-      "Obot Akara": {
-         "capital": "Obot Akara",
-         "population": "N/A"
-      },
-      "Okobo": {
-         "capital": "Okobo",
-         "population": "N/A"
-      },
-      "Onna": {
-         "capital": "Awa",
-         "population": "N/A"
-      },
-      "Oron": {
-         "capital": "Oron",
-         "population": "N/A"
-      },
-      "Oruk Anam": {
-         "capital": "Ikot Ibritam",
-         "population": "N/A"
-      },
-      "Udung Uko": {
-         "capital": "Okopedi",
-         "population": "N/A"
-      },
-      "Ukanafun": {
-         "capital": "Ukanafun",
-         "population": "N/A"
-      },
-      "Uruan": {
-         "capital": "Uruan",
-         "population": "N/A"
-      },
-      "Urue-Offong/Oruko": {
-         "capital": "Okopedi",
-         "population": "N/A"
-      },
-      "Uyo": {
-         "capital": "Uyo",
-         "population": "N/A"
-      }
-   }
-}
-{
-  "Anambra": [
-    "Aguata",
-    "Awka North",
-    "Awka South",
-    "Anambra East",
-    "Anambra West",
-    "Anaocha",
-    "Ayamelum",
-    "Dunukofia",
-    "Ekwusigo",
-    "Idemili North",
-    "Idemili South",
-    "Ihiala",
-    "Njikoka",
-    "Nnewi North",
-    "Nnewi South",
-    "Ogbaru",
-    "Onitsha North",
-    "Onitsha South",
-    "Orumba North",
-    "Orumba South",
-    "Oyi"
-  ]
-}
-{
-  "Bauchi State": {
-    "Alkaleri": {
-      "capital": "Alkaleri"
-    },
-    "Bauchi": {
-      "capital": "Bauchi"
-    },
-    "Bogoro": {
-      "capital": "Bogoro"
-    },
-    "Damban": {
-      "capital": "Damban"
-    },
-    "Darazo": {
-      "capital": "Darazo"
-    },
-    "Dass": {
-      "capital": "Dass"
-    },
-    "Ganjuwa": {
-      "capital": "Ganjuwa"
-    },
-    "Giade": {
-      "capital": "Giade"
-    },
-    "Itas/Gadau": {
-      "capital": "Itas/Gadau"
-    },
-    "Jama'are": {
-      "capital": "Jama'are"
-    },
-    "Katagum": {
-      "capital": "Katagum"
-    },
-    "Kirfi": {
-      "capital": "Kirfi"
-    },
-    "Misau": {
-      "capital": "Misau"
-    },
-    "Ningi": {
-      "capital": "Ningi"
-    },
-    "Shira": {
-      "capital": "Shira"
-    },
-    "Tafawa Balewa": {
-      "capital": "Tafawa Balewa"
-    },
-    "Toro": {
-      "capital": "Toro"
-    },
-    "Warji": {
-      "capital": "Warji"
-    },
-    "Zaki": {
-      "capital": "Zaki"
-    }
-  }
-}
-{
-  "Bayelsa": [
-    "Brass",
-    "Ekeremor",
-    "Kolokuma/Opokuma",
-    "Nembe",
-    "Ogbia",
-    "Sagbama",
-    "Southern Ijaw",
-    "Yenagoa"
-  ]
-}
-{
-  "Benue State LGAs": [
-    {
-      "LGA": "Ado",
-      "Capital": "Ado"
-    },
-    {
-      "LGA": "Agatu",
-      "Capital": "Obagaji"
-    },
-    {
-      "LGA": "Apa",
-      "Capital": "Ugbokpo"
-    },
-    {
-      "LGA": "Buruku",
-      "Capital": "Buruku"
-    },
-    {
-      "LGA": "Gboko",
-      "Capital": "Gboko"
-    },
-    {
-      "LGA": "Guma",
-      "Capital": "Gbajimba"
-    },
-    {
-      "LGA": "Gwer East",
-      "Capital": "Aliade"
-    },
-    {
-      "LGA": "Gwer West",
-      "Capital": "Naka"
-    },
-    {
-      "LGA": "Katsina-Ala",
-      "Capital": "Katsina-Ala"
-    },
-    {
-      "LGA": "Konshisha",
-      "Capital": "Tse-Agberagba"
-    },
-    {
-      "LGA": "Kwande",
-      "Capital": "Adikpo"
-    },
-    {
-      "LGA": "Logo",
-      "Capital": "Ugbokolo"
-    },
-    {
-      "LGA": "Makurdi",
-      "Capital": "Makurdi"
-    },
-    {
-      "LGA": "Obi",
-      "Capital": "Owukpa"
-    },
-    {
-      "LGA": "Ogbadibo",
-      "Capital": "Orokam"
-    },
-    {
-      "LGA": "Ohimini",
-      "Capital": "Idekpa"
-    },
-    {
-      "LGA": "Oju",
-      "Capital": "Oju"
-    },
-    {
-      "LGA": "Okpokwu",
-      "Capital": "Okpoga"
-    },
-    {
-      "LGA": "Oturkpo",
-      "Capital": "Oturkpo"
-    },
-    {
-      "LGA": "Tarka",
-      "Capital": "Wannune"
-    },
-    {
-      "LGA": "Ukum",
-      "Capital": "Ukum"
-    },
-    {
-      "LGA": "Ushongo",
-      "Capital": "Lessel"
-    },
-    {
-      "LGA": "Vandeikya",
-      "Capital": "Vandeikya"
-    }
-  ]
-}
-{
-  "Borno State": {
-    "Local Government Areas": [
-      "Abadam",
-      "Askira/Uba",
-      "Bama",
-      "Bayo",
-      "Biu",
-      "Chibok",
-      "Damboa",
-      "Dikwa",
-      "Gubio",
-      "Guzamala",
-      "Gwoza",
-      "Hawul",
-      "Jere",
-      "Kaga",
-      "Kala/Balge",
-      "Konduga",
-      "Kukawa",
-      "Kwaya Kusar",
-      "Mafa",
-      "Magumeri",
-      "Maiduguri",
-      "Marte",
-      "Mobbar",
-      "Monguno",
-      "Ngala",
-      "Nganzai",
-      "Shani"
-    ]
-  }
-}
-{
-  "Cross River State": {
-    "Akpabuyo": {},
-    "Odukpani": {},
-    "Akamkpa": {},
-    "Biase": {},
-    "Abi": {},
-    "Ikom": {},
-    "Yarkur": {},
-    "Odubra": {},
-    "Boki": {},
-    "Ogoja": {},
-    "Yala": {},
-    "Obanliku": {},
-    "Obudu": {},
-    "Bekwarra": {},
-    "Obubra": {},
-    "Etung": {},
-    "Iboko": {},
-    "Obudu": {},
-    "Calabar South": {},
-    "Calabar Municipality": {},
-    "Bakassi": {}
-  }
+
 }
 
 {
-  "Delta State": {
-    "Local Government Areas": [
-      "Aniocha North",
-      "Aniocha South",
-      "Bomadi",
-      "Burutu",
-      "Ethiope East",
-      "Ethiope West",
-      "Ika North East",
-      "Ika South",
-      "Isoko North",
-      "Isoko South",
-      "Ndokwa East",
-      "Ndokwa West",
-      "Okpe",
-      "Oshimili North",
-      "Oshimili South",
-      "Patani",
-      "Sapele",
-      "Udu",
-      "Ughelli North",
-      "Ughelli South",
-      "Ukwuani",
-      "Uvwie",
-      "Warri North",
-      "Warri South",
-      "Warri South West"
+    "Benue State LGAs": [
+        {
+            "LGA": "Ado",
+            "Capital": "Ado"
+        },
+        {
+            "LGA": "Agatu",
+            "Capital": "Obagaji"
+        },
+        {
+            "LGA": "Apa",
+            "Capital": "Ugbokpo"
+        },
+        {
+            "LGA": "Buruku",
+            "Capital": "Buruku"
+        },
+        {
+            "LGA": "Gboko",
+            "Capital": "Gboko"
+        },
+        {
+            "LGA": "Guma",
+            "Capital": "Gbajimba"
+        },
+        {
+            "LGA": "Gwer East",
+            "Capital": "Aliade"
+        },
+        {
+            "LGA": "Gwer West",
+            "Capital": "Naka"
+        },
+        {
+            "LGA": "Katsina-Ala",
+            "Capital": "Katsina-Ala"
+        },
+        {
+            "LGA": "Konshisha",
+            "Capital": "Tse-Agberagba"
+        },
+        {
+            "LGA": "Kwande",
+            "Capital": "Adikpo"
+        },
+        {
+            "LGA": "Logo",
+            "Capital": "Ugbokolo"
+        },
+        {
+            "LGA": "Makurdi",
+            "Capital": "Makurdi"
+        },
+        {
+            "LGA": "Obi",
+            "Capital": "Owukpa"
+        },
+        {
+            "LGA": "Ogbadibo",
+            "Capital": "Orokam"
+        },
+        {
+            "LGA": "Ohimini",
+            "Capital": "Idekpa"
+        },
+        {
+            "LGA": "Oju",
+            "Capital": "Oju"
+        },
+        {
+            "LGA": "Okpokwu",
+            "Capital": "Okpoga"
+        },
+        {
+            "LGA": "Oturkpo",
+            "Capital": "Oturkpo"
+        },
+        {
+            "LGA": "Tarka",
+            "Capital": "Wannune"
+        },
+        {
+            "LGA": "Ukum",
+            "Capital": "Ukum"
+        },
+        {
+            "LGA": "Ushongo",
+            "Capital": "Lessel"
+        },
+        {
+            "LGA": "Vandeikya",
+            "Capital": "Vandeikya"
+        }
     ]
-  }
+}
+{
+    "Borno State": {
+        "Local Government Areas": [
+            "Abadam",
+            "Askira/Uba",
+            "Bama",
+            "Bayo",
+            "Biu",
+            "Chibok",
+            "Damboa",
+            "Dikwa",
+            "Gubio",
+            "Guzamala",
+            "Gwoza",
+            "Hawul",
+            "Jere",
+            "Kaga",
+            "Kala/Balge",
+            "Konduga",
+            "Kukawa",
+            "Kwaya Kusar",
+            "Mafa",
+            "Magumeri",
+            "Maiduguri",
+            "Marte",
+            "Mobbar",
+            "Monguno",
+            "Ngala",
+            "Nganzai",
+            "Shani"
+        ]
+    }
+}
+{
+    "Cross River State": {
+        "Akpabuyo",
+        "Odukpani",
+        "Akamkpa",
+        "Biase",
+        "Abi",
+        "Ikom",
+        "Yarkur",
+        "Odubra",
+        "Boki",
+        "Ogoja",
+        "Yala",
+        "Obanliku",
+        "Obudu",
+        "Bekwarra",
+        "Obubra",
+        "Etung",
+        "Iboko",
+        "Obudu",
+        "Calabar South",
+        "Calabar Municipality",
+        "Bakassi"
+    }
 }
 
+{
+    "Delta State": {
+        "Local Government Areas": [
+            "Aniocha North",
+            "Aniocha South",
+            "Bomadi",
+            "Burutu",
+            "Ethiope East",
+            "Ethiope West",
+            "Ika North East",
+            "Ika South",
+            "Isoko North",
+            "Isoko South",
+            "Ndokwa East",
+            "Ndokwa West",
+            "Okpe",
+            "Oshimili North",
+            "Oshimili South",
+            "Patani",
+            "Sapele",
+            "Udu",
+            "Ughelli North",
+            "Ughelli South",
+            "Ukwuani",
+            "Uvwie",
+            "Warri North",
+            "Warri South",
+            "Warri South West"
+        ]
+    }
+}
